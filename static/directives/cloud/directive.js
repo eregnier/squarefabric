d23ffd15d10879148c78131c1ad16b5cb94fff87
utopman <utopman@gmail.com>
2016-01-05 12:24:55 +0100

    use directive

---
+++ static/directives/cloud/directive.js
@@ -0,0 +1,6 @@
+angular.module('squarefabricApp').directive('sfcloud', function() {
+    return {
+        templateUrl: 'static/directives/cloud/template.html',
+        link : function (scope, element, attrs) {}
+    }
+});
---
+++ static/directives/project/directive.js
@@ -0,0 +1,6 @@
+angular.module('squarefabricApp').directive('sfproject', function() {
+    return {
+        templateUrl: 'static/directives/project/template.html',
+        link : function (scope, element, attrs) {}
+    }
+});
