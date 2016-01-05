94505af74cfdbc86ebce110bcfdd2dd9f9b51b10
utopman <utopman@gmail.com>
2016-01-05 09:57:28 +0100

    started dispatch code into directives

---
+++ static/directives/menu/directive.js
@@ -0,0 +1,6 @@
+angular.module('squarefabricApp').directive('sfmenu', function() {
+    return {
+        templateUrl: 'static/directives/menu/template.html',
+        link : function (scope, element, attrs) {}
+    }
+});
---
+++ static/directives/pieceslist/directive.js
@@ -0,0 +1,6 @@
+angular.module('squarefabricApp').directive('sfpieceslist', function() {
+    return {
+        templateUrl: 'static/directives/pieceslist/template.html',
+        link : function (scope, element, attrs) {}
+    }
+});
