0df03a9d31fccd15b0219d2f73e46819643b8fbf
Eric Régnier <utopman@gmail.com>
2016-09-18 15:22:43 +0200

    change display lib

--- static/directives/layout/directive.js
+++ static/directives/layout/directive.js
@@ -2,55 +2,56 @@ angular.module('squarefabricApp').directive('sflayout', function(optimize) {
     return {
         templateUrl: 'static/directives/layout/template.html',
         link : function (scope, element, attrs) {
-
             scope.rectangles = [];
 
 
-            scope.repaint = function(blocks) {
+            var repaint = function(blocks, s, height) {
                 var block,
                     coeff = scope.coefficient,
                     colors = [ "#FFF7A5", "#FFA5E0", "#A5B3FF", "#BFFFA5", "#FFCBA5" ];
 
-                //window.$context = element.find('canvas')[0].getContext('2d');
-                element.find('canvas').attr('height', scope.coefficient * scope.maxHeight);
-                //element.find('canvas')[0].getContext('2d').innerHeight = scope.maxHeight;
-                //element.find('canvas').height(scope.coefficient * scope.maxHeight);
+                s.rect(0, 0, scope.$parent.currentProject.laize  * coeff, height  * coeff).attr({
+                    stroke: '#AAA',
+                    fill: '#EEEEDD'
+                });
 
                 for (var n = 0 ; n < blocks.length ; n++) {
                     block = blocks[n];
                     if (block.fit) {
-                        var rectangle = new paper.Rectangle(
-                            block.fit.x * coeff,
-                            block.fit.y * coeff,
-                            block.w * coeff,
-                            block.h * coeff
-                        );
-                        var path = paper.Path.Rectangle(rectangle);
-                        path.strokeColor = '#555555';
-                        path.fillColor = colors[n % colors.length];
-                        scope.rectangles.push(path);
+                    var rectangle = s.rect(
+                        block.fit.x * coeff,
+                        block.fit.y * coeff,
+                        block.w * coeff,
+                        block.h * coeff
+                    );
+                    rectangle.attr({
+                        fill: colors[n % colors.length],
+                        stroke: '#555'
+                    });
 
-                        var text = new paper.PointText(
-                            new paper.Point(block.fit.x * coeff + 3, (block.fit.y + block.h / 2) * coeff - 15)
-                        );
-                        text.content = block.name + '\nH : ' + block.h + '\nL : ' + block.w ;
-                        text.fillColor = '#555555';
-
+                    var text = block.name + '\nH : ' + block.h + '\nL : ' + block.w;
+                    s.text(block.fit.x  * coeff + 3, (block.fit.y + block.h / 2) * coeff - 15, text).attr({
+                        fill: '#555'
+                    });
 
                     }
                 }
-                paper.view.draw();
             };
 
-            var laize = scope.$parent.currentProject.laize,
-                canvas = element.find('canvas');
-            scope.coefficient = canvas.width() / laize;
-            paper.setup(canvas[0]);
+            scope.$on('setMaxHeight', function (e, height) {
+                var s = Snap($('body').width(), height * scope.coefficient);
+                repaint(scope.$parent.currentProject.pieces, s, height);
+            });
 
-            var packer = new Packer(laize, 1000);
+            var laize = scope.$parent.currentProject.laize;
+            scope.coefficient = $('body').width() / laize;
+
+            var packer = new Packer(laize, 100000);
             optimize.optimize(scope.$parent.currentProject.pieces, laize);
-            scope.repaint(scope.$parent.currentProject.pieces);
+
+
+
         }
 
-    }
+    };
 });
